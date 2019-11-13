87b99e2e73d0bb0116e6e424933379ebaf31b378
kazz <saikyodanzai000@gmail.com>
2019-11-13 16:17:18 +0900

    modify

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
     # バリデーション
     validates :name, length: { in: 1..20 }
     validates :edit_pass, length: { in: 1..12 }
-    validates :title, length: { in: 4..32 }
+    validates :title, length: { in: 4..32 }, allow_nil: true
 
     is_impressionable counter_cache: true
-    
+
     belongs_to :category
 end
